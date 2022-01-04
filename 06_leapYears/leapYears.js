d032199c37788364732c3d4d553fda535455e772
Dev Wulf <devinlwulf@gmail.com>
2022-01-03 22:05:18 -0600

    Update leapYears() in 06_leapYears; ftco(),ctof() in tempconversion. All Tests Passed.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
+function leapYears(year) {
+    if (year % 4 === 0 && year % 100 !== 0) {
+        return true;
+    } else if(year % 400 ===0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
-};
+leapYears(1996);
+leapYears(1997);
+leapYears(34992);
+leapYears(1900);
+leapYears(1600);
+leapYears(700);
 
 // Do not edit below this line
 module.exports = leapYears;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,24 @@
-const ftoc = function() {
+// Functino F to C
+function ftoc(far) {
+  const cel = Math.round(((far - 32) * (5/9)) * 10) / 10;
+  return cel;
+}
 
-};
+// Function C to F
+function ctof(cel) {
+  const far = Math.round(((cel * (9/5)) + 32) * 10) / 10;
+  return far;
+}
 
-const ctof = function() {
+// Fahrenheit to Celsius;
+ftoc(32);
+ftoc(100);
+ftoc(-100);
 
-};
+// Celcius to Fahrenheit;
+ctof(0);
+ctof(73.2);
+ctof(-10);
 
 // Do not edit below this line
 module.exports = {
